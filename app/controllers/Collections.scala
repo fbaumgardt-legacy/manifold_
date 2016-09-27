b9ce8d3f7be67011addb1b6546e482e4b06b3b34
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-09-26 22:43:22 -0400

    Return passage from Collections.read using models.URN

--- app/controllers/Collections.scala
+++ app/controllers/Collections.scala
@@ -1,16 +1,36 @@
 package controllers
 
-import models.{Record, URN}
+import javax.inject.{Inject, Singleton}
+
+import models._
+import models.JsonFormats._
 import org.slf4j.{Logger, LoggerFactory}
-import play.api.mvc.{Action, Controller}
-import play.modules.reactivemongo.MongoController
+import play.api.libs.json.Json
+import play.api.mvc._
+import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
+import play.modules.reactivemongo.json._
+import reactivemongo.api.Cursor
+import reactivemongo.play.json.collection.JSONCollection
+import services.{SimpleUUIDGenerator, UUIDGenerator}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 /**
   * Created by fbaumgardt on 31.05.16.
   */
-class Collections extends Controller { //with MongoController {
+class Collections @Inject() (
+                              val uuidGenerator: UUIDGenerator,
+                              val reactiveMongoApi: ReactiveMongoApi
+                            )
+  extends Controller
+    with MongoController
+    with ReactiveMongoComponents
+{
 
   private final val logger: Logger = LoggerFactory.getLogger(classOf[Application])
+
+  def collection: JSONCollection = db.collection[JSONCollection]("greek")
 
   def index = Action {
     logger.debug("Index requested.")
@@ -22,9 +42,31 @@ class Collections extends Controller { //with MongoController {
     Ok("200")
   }
 
-  def read(id:URN) = Action { // Int
-    logger.debug(id.toString)
-    Ok("200")
+  def read(id:URN) = Action.async { // Int
+
+    val query = Json.obj(
+      "_id.scheme" -> "urn",
+      "_id.namespace"->id.namespace,
+      "_id.domain"->id.domain,
+      "_id.textgroup"->id.textgroup,
+      "_id.work"->id.work,
+      "_id.edition"->id.edition,
+      "_id.passage"->id.passage
+    )
+
+    val cursor: Cursor[Sentence] = collection.
+      // find all people with name `name`
+      find(query).
+      // perform the query and get a cursor of JsObject
+      cursor[Sentence]
+
+    // gather all the JsObjects in a list
+    val futureUsersList: Future[List[Sentence]] = cursor.collect[List]()
+
+    // everything's ok! Let's reply with the array
+    futureUsersList.map { persons =>
+      Ok(Json.toJson(persons))
+    }
   }
 
   def update(record:Record[Int]) = Action {
